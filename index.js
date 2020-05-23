904332bfaee78ce0a7e0c80f6390858fe0b8f3d8
BurnTheWitch21 <witchtheburn21@gmail.com>
2020-05-23 20:13:44 +0200

    removed token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 
 const bot = new Discord.Client();
-const token = 'NzA2Nzg0NjAzODc2MDk4MTE5.Xr7_ig.PqKGhWRP2OjYwZsp0JkX9eoZfUU';
+const token = '';
 const PREFIX = '%';
 var version = '1.0';
 var servers = {};
